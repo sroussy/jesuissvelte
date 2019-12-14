46ccf2248bbc8d713206fe4d583e18e6db8dd2f9
Samuel Roussy <samuel.duberoussy@gmail.com>
2019-12-14 18:31:05 -0500

    replace tabs by spaces

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,35 @@
 <script>
-	export let name;
+    export let name;
 </script>
 
 <main>
-	<h1>Is it you {name}<span class="spaceBefore">?!</span></h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>Is it you {name}<span class="spaceBefore">?!</span></h1>
+    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		/*color: #ff3e00;*/
+    h1 {
+        /*color: #ff3e00;*/
         color: #ff0057;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: bold;
-	}
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: bold;
+    }
 
     .spaceBefore{
         margin-left: 0.2em;
     }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
